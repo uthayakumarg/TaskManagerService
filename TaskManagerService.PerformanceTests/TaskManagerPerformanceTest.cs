efd0a3b395842a821992e037ae44bf3006c8c901
uthayakumarg <uthayakumarg@gmail.com>
2018-08-31 21:51:03 +0530

    Performance tests added

--- TaskManagerService.PerformanceTests/TaskManagerPerformanceTest.cs
+++ TaskManagerService.PerformanceTests/TaskManagerPerformanceTest.cs
@@ -1,34 +1,69 @@
 ﻿using NBench;
-using NBench.Util;
+using System.Collections.Generic;
+using TaskManagerService.BusinessLayer;
+using TaskManagerService.Entity;
+using TaskManagerService.DataLayer;
+using System;
 
 namespace TaskManagerService.PerformanceTests
 {
     public class TaskManagerPerformanceTest
     {
-        private Counter _counter;
+        private ITaskManagerBL _taskService;
+        private TaskEntity _newTask;
+        private TaskEntity _taskToBeUpdated;
 
         [PerfSetup]
         public void Setup(BenchmarkContext context)
         {
-            _counter = context.GetCounter("TestCounter");
+            ITasksRepository _repo = new TaskManagerDataAccess();
+            _taskService = new TaskManagerBL(_repo);
+            _newTask = new TaskEntity { TaskId = 0, TaskName = "Coding", ParentId = 1, ParentName = "Project Task", Priority = 2, ActiveInd = "Y", StartDate = new DateTime(2018, 04, 21), EndDate = new DateTime(2018, 08, 26), CreatedBy = "PerfTest" };
+            _taskToBeUpdated = new TaskEntity { TaskId = 1, TaskName = "Analysis", ParentId = 1, ParentName = "Project Task", Priority = 3, ActiveInd = "Y", StartDate = new DateTime(2018, 04, 18), EndDate = new DateTime(2018, 08, 20) };
         }
 
-        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
-            NumberOfIterations = 3, RunMode = RunMode.Throughput,
-            RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
-        [CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 10000000.0d)]
-        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThanOrEqualTo, ByteConstants.ThirtyTwoKb)]
-        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
-        public void Benchmark()
+        [PerfBenchmark(NumberOfIterations =1, RunMode =RunMode.Throughput, TestMode =TestMode.Test, SkipWarmups =true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds =2000)]
+        public void Get_All_Tasks_Elapsed_Time()
         {
-            _counter.Increment();
+            var result = _taskService.GetAllTasks();
         }
 
-        [PerfCleanup]
-        public void Cleanup()
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput, TestMode = TestMode.Test, RunTimeMilliseconds = 5000, SkipWarmups = true)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.GreaterThan, ByteConstants.SixteenKb)]
+        public void Get_All_Tasks_Memory_Consumed()
         {
-            // does nothing
+            var result = _taskService.GetAllTasks();
         }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput, TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 1000)]
+        public void Add_Task_Elapsed_Time()
+        {
+            _taskService.AddTask(_newTask);
+        }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput, TestMode = TestMode.Test, RunTimeMilliseconds = 5000, SkipWarmups = true)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.GreaterThan, ByteConstants.SixteenKb)]
+        public void Add_Task_Memory_Consumed()
+        {
+            _taskService.AddTask(_newTask);
+        }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput, TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 2000)]
+        public void Update_Task_Elapsed_Time()
+        {
+            _taskService.UpdateTask(_taskToBeUpdated);
+        }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput, TestMode = TestMode.Test, RunTimeMilliseconds = 5000, SkipWarmups = true)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.GreaterThan, ByteConstants.SixteenKb)]
+        public void Update_Task_Memory_Consumed()
+        {
+            _taskService.UpdateTask(_taskToBeUpdated);
+        }
+
 
     }
 }
